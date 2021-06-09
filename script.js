61b54c7989904de6acb3bc43112291b2f3dbbc06
ultrasoft011 <ultrasoft.011@gmail.com>
2021-06-09 09:07:07 -0500

    neighbor call Ajax

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const countriesContainer = document.querySelector('.countries');
 // Function to render the country
 // Optional: work with the JSON information to fit some data
 const renderCountry = function (data) {
-const html = `
+  const html = `
   <article class="country">
     <img class="country__img" src="${data.flag}" />
     <div class="country__data">
@@ -18,18 +18,18 @@ const html = `
         data.population / 1000000
       ).toFixed(1)} Millions</p>
       <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-      <p class="country__row"><span>💰</span>${
-        data.currencies[0].name
-      }</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
     </div>
   </article>`;
 
-countriesContainer.insertAdjacentHTML('beforeend', html);
-countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
 };
 
 // Function to request the country and neighbor to an API
-const getCountry = function (country) {
+const getNeighborAndCountry = function (country) {
+
+  //Ajax call country 1
   // 1. Invoke the XMLHttpRequest method
   const request = new XMLHttpRequest();
   // 2. Invoke the open() method and pass as arguments 'GET' and 'url of the API'
@@ -40,13 +40,30 @@ const getCountry = function (country) {
   request.addEventListener('load', function () {
     // 5. The load will bring a JSON format (string) that can be converted in an Object with parse
     // Optional: destructuring the array [] to get only the object
+
     const [data] = JSON.parse(this.responseText);
     console.log(data);
-    renderCountry(data);
-  });
-  };
 
-getCountry('Colombia');
-getCountry('Dominican Republic');
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbor country 2
+    const [neighbor] = data.borders;
+    if(!neighbor) return;
+
+    // Ajax call country 2
+
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbor}`);
+    request2.send();
+    // Callback function inside another one
+    request2.addEventListener('load', function() {
+      console.log(this.responseText);
+    })
+
+  });
+};
+
+getNeighborAndCountry('Colombia');
 
 ///////////////////////////////////////
