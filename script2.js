a866a2020591481fe17cb8bd68dc98894f6d833d
ultrasoft011 <ultrasoft.011@gmail.com>
2021-06-10 16:33:54 -0500

    coding challenge: ajax call

--- script2.js
+++ script2.js
@@ -7,21 +7,31 @@ const countriesContainer = document.querySelector('.countries');
 // Fetch returns a promise <pending>
 
 const getCountry = function (country) {
-// Country 1
-  fetch(`https://restcountries.eu/rest/v2/name/${country}`).then(res => res.json())
+  // Country 1
+  fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+    .then(res => {
+      console.log(res);
+      // To handle error manually 
+      if (!res.ok) throw new Error(`Country not found' ${res.ok})`);
+
+      return res.json();
+    })
     // This then method returns a new promise
-    .then(data => { 
-        renderCountry(data[0]);
-        const neighbor = data[0].borders[0];
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbor = data[0].borders[0];
 
-        if (!neighbor) return;
+      if (!neighbor) return;
 
-    //Country 2
-    return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbor}`);
+      //Country 2: return the promise and handle it outside the functio to avoid the callback hell
+      return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbor}`);
     })
     .then(response => response.json())
-    .then(data => renderCountry(data, 'neighbor'));
-}
+    .then(data => renderCountry(data))
+    .catch(err => {
+      console.log(`${err}`);
+    });
+};
 
 getCountry('Colombia');
 
@@ -43,4 +53,4 @@ const renderCountry = function (data) {
 
   countriesContainer.insertAdjacentHTML('beforeend', html);
   countriesContainer.style.opacity = 1;
-}
+};
