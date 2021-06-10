aea2e275f13c5e397fa45b5040b55387aea38ef4
ultrasoft011 <ultrasoft.011@gmail.com>
2021-06-10 17:50:26 -0500

    coding challenge: catching error

--- coding-challenge.js
+++ coding-challenge.js
@@ -22,7 +22,10 @@ GOOD LUCK 😀
 
 const whereAmI = function (lat, lng) {
   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
-    .then(response => response.json())
+    .then(response => {
+        if(!response.ok) throw new Error(`Problem with geocoding ${response.status}`);
+        return response.json();
+    })
     .then(data => {
       console.log(data);
       console.log(`You are in ${data.city}, ${data.country}`);
@@ -32,4 +35,6 @@ const whereAmI = function (lat, lng) {
     });
 };
 
+whereAmI(52.508, 13.381);
 whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
